31d0cf20f42713ca8082a11da415aeaa1e72a6eb
Andrew Minion <andrew.minion@luminfire.com>
2021-05-06 15:26:58 -0500

    simplify routes

--- routes/teams.php
+++ routes/teams.php
@@ -5,12 +5,10 @@ use BrilliantPortal\Framework\Http\Middleware\EnsureHasNoTeam;
 use Illuminate\Support\Facades\Route;
 
 Route::name('brilliant-portal-framework.teams.')
+    ->middleware(['web', EnsureHasNoTeam::class])
     ->group(function () {
-        Route::middleware(['web', EnsureHasNoTeam::class])
-            ->group(function () {
-                Route::get('teams/create-first', [TeamController::class, 'create'])->name('create-first');
-                Route::post('teams/create-first', [TeamController::class, 'store'])->name('store-first');
-            });
+        Route::get('teams/create-first', [TeamController::class, 'create'])->name('create-first');
+        Route::post('teams/create-first', [TeamController::class, 'store'])->name('store-first');
 
         Route::get('teams/already-invited', [TeamController::class, 'alreadyInvited'])->name('already-invited');
     });
