bae437d8e54c9e846a211eff9cda535401b3ba58
Andrew Minion <andrew.minion@luminfire.com>
2021-05-12 09:03:30 -0500

    track modified vendor files

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -15,6 +15,8 @@ class InstallCommand extends Command
 
     public $description = 'Install all of the resources and components';
 
+    protected $changedVendorFiles = [];
+
     public function handle()
     {
         /**
@@ -32,14 +34,18 @@ class InstallCommand extends Command
 
         if ($this->option('teams')) {
             // Actions.
+            $this->checkFileHash('vendor/laravel/jetstream/stubs/app/Actions/Fortify/CreateNewUser.php', 'a40bf00dd23a574d2515df28ce35496e987c951722c880f0e70605a68f7b2d52');
             copy(__DIR__.'/../../stubs/app/Actions/Fortify/CreateNewUser.stub.php', app_path('Actions/Fortify/CreateNewUser.php'));
 
             // Migrations and Models.
+            $this->checkFileHash('vendor/laravel/jetstream/stubs/app/Models/Team.php', '6da867dcf458b38d313517a529abec1852a4f9ffdeae5ab667004b3de198b53f');
+            $this->checkFileHash('vendor/laravel/jetstream/stubs/app/Models/UserWithTeams.php', '7066970d21811528c0a502aefb6da0f616b52b86180b093ecffa665db5a88658');
             copy(__DIR__.'/../../stubs/database/migrations/2015_01_01_000000_add_super_admins.stub.php', base_path('database/migrations/2015_01_01_000000_add_super_admins.php'));
             copy(__DIR__.'/../../stubs/app/Models/Team.stub.php', app_path('Models/Team.php'));
             copy(__DIR__.'/../../stubs/app/Models/UserWithTeams.stub.php', app_path('Models/User.php'));
 
             // Providers.
+            $this->checkFileHash('vendor/laravel/jetstream/stubs/app/Providers/AuthServiceProvider.php', 'f1d80a0c8a3b252187173c08952a4801683aa71136805d8b0ed100b33935fd7b');
             copy(__DIR__.'/../../stubs/app/Providers/AuthServiceProvider.stub.php', app_path('Providers/AuthServiceProvider.php'));
 
             // Tests.
@@ -92,6 +98,7 @@ class InstallCommand extends Command
          * OpenAPI docs.
          */
         if ($this->option('api')) {
+            $this->checkFileHash('vendor/vyuldashev/laravel-openapi/config/openapi.php', '83d3ef3b1887c1e11dace8375abf047c640678944d2b54fa9679a11927879601');
             copy(__DIR__.'/../../stubs/config/openapi.stub.php', config_path('openapi.php'));
 
             $this->replaceInFile('Jetstream::role(\'admin\', __(\'Administrator\'), [', 'Jetstream::role(\'admin\', __(\'Administrator\'), [
@@ -108,6 +115,24 @@ class InstallCommand extends Command
             copy(__DIR__.'/../../stubs/tests/Api/V1UsersTest.stub.php', base_path('tests/Feature/Api/V1UsersTest.php'));
             copy(__DIR__.'/../../stubs/tests/Api/V1TeamsTest.stub.php', base_path('tests/Feature/Api/V1TeamsTest.php'));
         }
+
+        /**
+         * Modified vendor files.
+         */
+        if ($this->changedVendorFiles) {
+            $this->warn('Some of the vendor files overridden by BrilliantPortal Framework have been modified. Please open an issue and assign to the appropriate person.');
+            $this->warn('Click the link below to start an issue, then copy-and-paste the table into the issue dsecription.');
+            $this->newLine();
+            $this->line('https://git.luminfire.net/products/brilliantportal/brilliantportal-framework/-/issues/new?issue[title]=Modified vendor files&issue[description]=These vendor files have been modified:');
+            $this->table(
+                ['Modified Files'],
+                array_map(function ($file) {
+                    return [$file];
+                }, $this->changedVendorFiles)
+            );
+        }
+
+        $this->info('Done!');
     }
 
     /**
@@ -121,5 +146,24 @@ class InstallCommand extends Command
     protected function replaceInFile($search, $replace, $path)
     {
         file_put_contents($path, str_replace($search, $replace, file_get_contents($path)));
+    }
+
+    /**
+     * Check vendor file against known hash.
+     *
+     * @since 1.1.0
+     *
+     * @param string $path
+     * @param string $expectedHash
+     *
+     * @return void
+     */
+    protected function checkFileHash($path, $expectedHash)
+    {
+        $actualHash = hash('sha256', file_get_contents(base_path($path)));
+
+        if ($actualHash !== $expectedHash) {
+            $this->changedVendorFiles[] = $path;
+        }
     }
 }
