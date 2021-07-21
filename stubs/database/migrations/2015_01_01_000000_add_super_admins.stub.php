8474a21c66565d65171e1f1015e445bc7c9e8d7e
Andrew Minion <andrew.minion@luminfire.com>
2021-07-21 13:13:39 -0500

    move super-admin column

--- stubs/database/migrations/2015_01_01_000000_add_super_admins.stub.php
+++ stubs/database/migrations/2015_01_01_000000_add_super_admins.stub.php
@@ -14,7 +14,7 @@ class AddSuperAdmins extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_super_admin')->default(false);
+            $table->boolean('is_super_admin')->after('remember_token')->default(false);
         });
     }
 
