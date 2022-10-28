53d8c7ef24258f6fec0b841f588d5858550b956f
Andrew Minion <andrew.minion@luminfire.com>
2022-10-28 11:12:02 -0500

    create directory if necessary

--- src/Commands/PublishBrandingCommand.php
+++ src/Commands/PublishBrandingCommand.php
@@ -12,8 +12,14 @@ class PublishBrandingCommand extends Command
 
     public function handle()
     {
+        if (! file_exists(resource_path('views/vendor/jetstream/components/'))) {
+            mkdir(resource_path('views/vendor/jetstream/components/'), 0755, true);
+        }
+
         copy(base_path('vendor/laravel/jetstream/resources/views/components/application-logo.blade.php'), resource_path('views/vendor/jetstream/components/application-logo.blade.php'));
         copy(base_path('vendor/laravel/jetstream/resources/views/components/authentication-card-logo.blade.php'), resource_path('views/vendor/jetstream/components/authentication-card-logo.blade.php'));
         copy(base_path('vendor/laravel/jetstream/resources/views/components/application-mark.blade.php'), resource_path('views/vendor/jetstream/components/application-mark.blade.php'));
+
+        return self::SUCCESS;
     }
 }
