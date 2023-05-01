9468eb5fd5d7d981ad7952aac1d466b992ec6ec7
Andrew Minion <andrew.minion@luminfire.com>
2023-05-01 11:34:14 -0500

    simplify vite config

--- stubs/vite-standard.config.js
+++ stubs/vite-standard.config.js
@@ -1,8 +1,5 @@
-import fs from 'fs';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import {homedir} from 'os';
-import {resolve} from 'path';
 
 let host = 'localhost.test';
 
@@ -10,34 +7,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/update-timezone.js',
             ],
             refresh: true,
+            valetTls: host,
         }),
     ],
-
-    server: detectServerConfig(host),
 });
-
-function detectServerConfig(host) {
-    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
-    let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
-
-    if (!fs.existsSync(keyPath)) {
-        return {}
-    }
-
-    if (!fs.existsSync(certificatePath)) {
-        return {}
-    }
-
-    return {
-        hmr: {host},
-        host,
-        https: {
-            key: fs.readFileSync(keyPath),
-            cert: fs.readFileSync(certificatePath),
-        },
-    }
-}
