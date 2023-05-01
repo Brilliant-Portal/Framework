e1a26777e3aa9efe1528294d4614df0ab16549d0
Andrew Minion <andrew.minion@luminfire.com>
2023-05-01 16:43:52 -0500

    use request object

--- src/Http/Middleware/EnsureHasTeam.php
+++ src/Http/Middleware/EnsureHasTeam.php
@@ -4,27 +4,26 @@ namespace BrilliantPortal\Framework\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class EnsureHasTeam
 {
     public function handle(Request $request, Closure $next)
     {
-        if (! Auth::user()?->isMemberOfATeam()) {
+        if (! $request->user()?->isMemberOfATeam()) {
             return redirect()->route('brilliant-portal-framework.teams.create-first');
         }
 
-        $this->ensureOneOfTheTeamsIsCurrent();
+        $this->ensureOneOfTheTeamsIsCurrent($request);
 
         return $next($request);
     }
 
-    protected function ensureOneOfTheTeamsIsCurrent(): void
+    protected function ensureOneOfTheTeamsIsCurrent(Request $request): void
     {
-        if (! is_null(Auth::user()?->current_team_id)) {
+        if (filled($request->user()?->current_team_id)) {
             return;
         }
 
-        Auth::user()->switchTeam(Auth::user()->allTeams()->first());
+        $request->user()->switchTeam($request->user()->allTeams()->first());
     }
 }
