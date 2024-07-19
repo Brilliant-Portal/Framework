d320776ceef691ac00e1283f24d1844971a793c4
Andrew Minion <andrew.minion@luminfire.com>
2024-07-19 16:38:08 -0500

    add some removed authorization methods
    #56

--- src/Http/Controllers/Api/Controller.php
+++ src/Http/Controllers/Api/Controller.php
@@ -2,7 +2,9 @@
 
 namespace BrilliantPortal\Framework\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller as AppController;
+use Illuminate\Contracts\Auth\Access\Gate;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Str;
 
 /**
  * Team controller.
@@ -12,7 +14,7 @@ use App\Http\Controllers\Controller as AppController;
  * @property class-string $model
  * @property string $ability
  */
-class Controller extends AppController
+class Controller extends BaseController
 {
     /**
      * Set authorization for all methods.
@@ -22,5 +24,117 @@ class Controller extends AppController
     public function __construct()
     {
         $this->authorizeResource($this->model, $this->ability);
+    }
+
+    /**
+     * Authorize a given action for the current user.
+     *
+     * From Laravel < 11.
+     *
+     * @param  mixed  $ability
+     * @param  mixed|array  $arguments
+     * @return \Illuminate\Auth\Access\Response
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    protected function authorize($ability, $arguments = [])
+    {
+        [$ability, $arguments] = $this->parseAbilityAndArguments($ability, $arguments);
+
+        return app(Gate::class)->authorize($ability, $arguments);
+    }
+
+    /**
+     * Guesses the ability's name if it wasn't provided.
+     *
+     * @param  mixed  $ability
+     * @param  mixed|array  $arguments
+     * @return array
+     */
+    protected function parseAbilityAndArguments($ability, $arguments)
+    {
+        if (is_string($ability) && ! str_contains($ability, '\\')) {
+            return [$ability, $arguments];
+        }
+
+        $method = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3)[2]['function'];
+
+        return [$this->normalizeGuessedAbilityName($method), $ability];
+    }
+
+    /**
+     * Normalize the ability name that has been guessed from the method name.
+     *
+     * @param  string  $ability
+     * @return string
+     */
+    protected function normalizeGuessedAbilityName($ability)
+    {
+        $map = $this->resourceAbilityMap();
+
+        return $map[$ability] ?? $ability;
+    }
+
+    /**
+     * Authorize a resource action based on the incoming request.
+     *
+     * From Laravel < 11.
+     *
+     * @param  string|array  $model
+     * @param  string|array|null  $parameter
+     * @param  \Illuminate\Http\Request|null  $request
+     * @return void
+     */
+    protected function authorizeResource($model, $parameter = null, array $options = [], $request = null)
+    {
+        $model = is_array($model) ? implode(',', $model) : $model;
+
+        $parameter = is_array($parameter) ? implode(',', $parameter) : $parameter;
+
+        $parameter = $parameter ?: Str::snake(class_basename($model));
+
+        $middleware = [];
+
+        foreach ($this->resourceAbilityMap() as $method => $ability) {
+            $modelName = in_array($method, $this->resourceMethodsWithoutModels()) ? $model : $parameter;
+
+            $middleware["can:{$ability},{$modelName}"][] = $method;
+        }
+
+        foreach ($middleware as $middlewareName => $methods) {
+            $this->middleware($middlewareName, $options)->only($methods);
+        }
+    }
+
+    /**
+     * Get the map of resource methods to ability names.
+     *
+     * From Laravel < 11.
+     *
+     * @return array
+     */
+    protected function resourceAbilityMap()
+    {
+        return [
+            'index' => 'viewAny',
+            'show' => 'view',
+            'create' => 'create',
+            'store' => 'create',
+            'edit' => 'update',
+            'update' => 'update',
+            'destroy' => 'delete',
+        ];
+    }
+
+    /**
+     * Get the list of resource methods which do not have model parameters.
+     *
+     * From Laravel < 11.
+     *
+     * @return array
+     */
+    protected function resourceMethodsWithoutModels()
+    {
+        return ['index', 'create', 'store'];
     }
 }
