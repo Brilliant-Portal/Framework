37630c2fb29e6c010955da5ba8c86e82ccbc7567
Andrew Minion <andrew.minion@luminfire.com>
2024-07-19 11:53:56 -0500

    update stubs
    Part of #55

--- stubs/app/Models/User.stub.php
+++ stubs/app/Models/User.stub.php
@@ -39,6 +39,7 @@ class User extends Authenticatable
         'remember_token',
         'two_factor_recovery_codes',
         'two_factor_secret',
+        'is_super_admin',
     ];
 
     /**
@@ -60,6 +61,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_super_admin' => 'boolean',
         ];
     }
 }
